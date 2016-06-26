26ad3b4db9e9af755313221f24a938d335f02ee4
Siddharth Chugh <siddharthandroiddev@gmail.com>
2016-06-26 21:37:25 +0530

    DOne

--- app/src/main/java/com/example/richie/mybooks/Activities/AddBooksActivity.java
+++ app/src/main/java/com/example/richie/mybooks/Activities/AddBooksActivity.java
@@ -1,7 +1,12 @@
 package com.example.richie.mybooks.Activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.richie.mybooks.R;
 
@@ -11,5 +16,45 @@ public class AddBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_books);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.done:
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setMessage("Do you want to leave the screen with unsaved changes!");
+
+                alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                      //  startActivity(new Intent(AddBooksActivity.this, MainActivity.class));
+             finish();
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        onResume();
+                    }
+                });
+
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/richie/mybooks/Fragments/AddBookFragment.java
+++ app/src/main/java/com/example/richie/mybooks/Fragments/AddBookFragment.java
@@ -198,40 +198,41 @@ public class AddBookFragment extends Fragment implements View.OnClickListener{
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        switch (id){
-            case R.id.done:
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
-                alertDialogBuilder.setMessage("Do you want to leave the screen with unsaved changes!");
-
-                alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface arg0, int arg1) {
-                        getContext().startActivity(new Intent(getContext(), MainActivity.class));
-                    }
-                });
-
-                alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        onResume();
-                    }
-                });
-
-
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        int id = item.getItemId();
+//        switch (id){
+//            case R.id.done:
+//
+//                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+//                alertDialogBuilder.setMessage("Do you want to leave the screen with unsaved changes!");
+//
+//                alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface arg0, int arg1) {
+//                        getContext().startActivity(new Intent(getContext(), MainActivity.class));
+//
+//                    }
+//                });
+//
+//                alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                        onResume();
+//                    }
+//                });
+//
+//
+//                AlertDialog alertDialog = alertDialogBuilder.create();
+//                alertDialog.show();
+//
+//                break;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     public void onClick(View view) {
